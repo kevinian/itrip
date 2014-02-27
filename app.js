7067058792f33c5c15d3e59e5aca02f8706acc09
kevinian <kevinpaul_boa@hotmail.com>
2014-02-27 16:48:16 +0100

    http-proxy with current version support

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ http.createServer(mainApp).listen(mainApp.get('port'), function(){
 });
 
 /**
- * CMS
+ * Keystone CMS
  */
 // Simulate config options from your production environment by
 // customising the .env file in your project's root folder.
@@ -104,24 +104,31 @@ console.log('Keystone Listening on port 8082');
 
 /**
  * NodeBB
+ * TODO: integrate with nodeBB
  */
 
 
 /**
  * Proxy Server
+ * TODO: use `http-proxy` module as load balancer.
+ *       use `http-master` module as reverse proxy. [http-master](https://github.com/CodeCharmLtd/http-master)
  */
 var httpProxy = require('http-proxy');
 
-var options = {
-  hostnameOnly: true,
-  router: {
-    'itrip.com':                '127.0.0.1:8081',
-    'www.itrip.com':            '127.0.0.1:8081',
-    'dianping.itrip.com':       '127.0.0.1:8082',
-    'forum.itrip.com':          '127.0.0.1:8083',
-    'blog.itrip.com':           '127.0.0.1:8084',
-  }
-};
+var proxy = httpProxy.createProxyServer({});
 
-var proxyServer = httpProxy.createServer(options).listen(8080);
+var router = {
+  'itrip.com:8080':                'http://127.0.0.1:8081',
+  'www.itrip.com:8080':            'http://127.0.0.1:8081',
+  'dianping.itrip.com:8080':       'http://127.0.0.1:8082',
+  'forum.itrip.com:8080':          'http://127.0.0.1:8083',
+  'blog.itrip.com:8080':           'http://127.0.0.1:8084',
+}
+
+http.createServer(function(req, res) {
+  proxy.web(req, res, {
+    target: router[req.headers.host]
+  });
+}).listen(8080);
+
 console.log('Proxy Server Listening on port 8080');
